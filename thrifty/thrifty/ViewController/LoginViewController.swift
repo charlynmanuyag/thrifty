a068f95f98087b5be0dd62495e4306b60fb15da6
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2018-11-27 01:21:50 -0800

    set up firebase

--- thrifty/thrifty/ViewController/LoginViewController.swift
+++ thrifty/thrifty/ViewController/LoginViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var Username: LoginSignupTextField!
+    @IBOutlet weak var Password: LoginSignupTextField!
+    
+    var userEmail = ""
+    var userPassword = ""
+    
+    @IBAction func LoginPressed(_ sender: Any) {
+        guard let usernameText = Username.text else { return }
+        guard let passwordText = Password.text else { return }
+        
+        if usernameText == "" || passwordText == "" {
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields
+            let alertController = UIAlertController(title: "Log In Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            Auth.auth().signIn(withEmail: userEmail, password: userPassword) { (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "LoginToHomePage", sender: self)
+                }
+                    // email and password fields are not blank, let's try logging in the user!
+                    // you'll need to use `emailText` and `passwordText`, and a method found in this
+                    // api doc https://firebase.google.com/docs/auth/ios/start
+                    // if the error == nil, segue to the main page using `performSegue` with identifier
+                    // `segueLogInToMainPage`
+                    // if there is an error signing in (error != nil), present the following alert:
+                else {
+                    let alertController = UIAlertController(title: "Log In Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +69,16 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.Username {
+            if textField.text != nil {
+                self.userEmail = textField.text!
+            }
+        } else {
+            if textField.text != nil {
+                self.userPassword = textField.text!
+            }
+        }
+    }
 
 }
--- thrifty/thrifty/ViewController/SignupViewControllers/SignupViewController.swift
+++ thrifty/thrifty/ViewController/SignupViewControllers/SignupViewController.swift
@@ -7,11 +7,88 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class SignupViewController: UIViewController {
-
+class SignupViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstName: LoginSignupTextField!
+    @IBOutlet weak var lastName: LoginSignupTextField!
+    @IBOutlet weak var password: LoginSignupTextField!
+    @IBOutlet weak var confirmPassword: LoginSignupTextField!
+    @IBOutlet weak var email: LoginSignupTextField!
+    
+    var userEmail = ""
+    var first = ""
+    var last = ""
+    var userPassword = ""
+    var userVerifiedPassword = ""
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        guard let userEmail = email.text else { return }
+        guard let userPassword = password.text else { return }
+        guard let first = firstName.text else { return }
+        guard let last = lastName.text else { return }
+        guard let userVerifiedPassword = confirmPassword.text else { return }
+        
+        if userEmail == "" || userPassword == "" || first == "" || userVerifiedPassword == "" || last == ""  {
+            let alertController = UIAlertController(title: "Form Error.", message: "Please fill in form completely.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: userEmail, password: userPassword) { (user, error) in
+                if error == nil {
+                    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                    changeRequest?.displayName = userEmail
+                    changeRequest?.commitChanges { (error) in
+                        if error == nil {
+                            let alertController = UIAlertController(title: "Display Name Ceeated!", message: "Congrats! You have created a new Thrifty Account!", preferredStyle: .alert)
+                            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: {_ in self.performSegue(withIdentifier: "SignupToWelcome", sender: self)})
+                            alertController.addAction(defaultAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                        else {
+                            let alertController = UIAlertController(title: "Sign Up Error", message: error?.localizedDescription, preferredStyle: .alert)
+                            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            alertController.addAction(defaultAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                    // TO DO:
+                    // The user account has been successfully created. Now, update the user's name in
+                    // firebase and then perform a segue to the main page. Note, again, that this segue
+                    // already exists somewhere, just do some simple debugging to find the identifier.
+                    // Also, notify the user that the account has been successfully created before
+                    // performing the segue.
+                    guard (user?.user) != nil else { return }
+                    let alertController = UIAlertController(title: "New Account Created", message: "Congrats! You have created a new Thrifty Account!", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: {_ in self.performSegue(withIdentifier: "SignupToWelcome", sender: self)})
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                } else if userPassword != userVerifiedPassword {
+                    let alertController = UIAlertController(title: "Verification Error.", message: "The two passwords do not match.", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.confirmPassword.textColor = UIColor.red
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    let alertController = UIAlertController(title: "Sign Up Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstName.delegate = self
+        self.lastName.delegate = self
+        self.password.delegate = self
+        self.confirmPassword.delegate = self
+        self.email.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -31,5 +108,29 @@ class SignupViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.firstName {
+            if textField.text != nil {
+                self.first = textField.text!
+            }
+        } else if textField == self.lastName {
+            if textField.text != nil {
+                self.last = textField.text!
+            }
+        } else if textField == self.password {
+            if textField.text != nil {
+                self.userPassword = textField.text!
+            }
+        } else if textField == self.confirmPassword {
+            if textField.text != nil {
+                self.userVerifiedPassword = textField.text!
+            }
+        }
+        else if textField == self.email {
+            if textField.text != nil {
+                self.userEmail = textField.text!
+            }
+        }
+    }
 
 }
